7a25156880bb93d51cecf9ba092bced779c60397
Nilesh <nilesh.jha1@live.com>
2023-08-27 15:15:25 +0530

    Catalog API dev done, need to containerize

---
+++ src/Services/Catalog/Catalog.API/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string Id);
+        Task<IEnumerable<Product>>GetProductByName(string Name);
+        Task<IEnumerable<Product>>GetProductByCategory(string CategoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string Id);
+    }
+}
